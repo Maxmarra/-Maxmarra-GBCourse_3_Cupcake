ad74ed553c92024305d3dab1520855a6d26cc94c
Maxmarra <5919669@gmail.com>
2021-12-26 13:26:14 +0300

    use back button

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -2,21 +2,28 @@ package com.example.cupcake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
-{
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //п олучаем доступ к содержанию nav_graph.xml
+        //handling fragment title
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
-        //отображаем содержание android:label ... - в заголовке appbar
         setupActionBarWithNavController(navController)
+    }
+
+    //handling backbutton
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
 
