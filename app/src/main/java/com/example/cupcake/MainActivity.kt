c797ab2937bc3135099228163df2f36704773b54
Maxmarra <5919669@gmail.com>
2022-01-17 14:06:49 +0300

    adding navigation_01

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -10,12 +10,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //п олучаем доступ к содержанию nav_graph.xml
+        //получаем доступ к содержанию nav_graph.xml
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         //отображаем содержание android:label ... - в заголовке appbar
+        //This will do the following: Show a title in the app bar
+        // based off of the destination's label,
+        // and display the Up button whenever you're not on a top-level destination.
         setupActionBarWithNavController(navController)
     }
 }
