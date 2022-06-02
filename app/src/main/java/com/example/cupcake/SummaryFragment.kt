6a95a5fedd2c11016a278100e633b55c1f807104
Maxmarra <5919669@gmail.com>
2022-06-02 11:44:28 +0300

    drilling_1

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.cupcake.databinding.FragmentSummaryBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * [SummaryFragment] contains a summary of the order details with a button to share the order
@@ -14,6 +17,7 @@ import com.example.cupcake.databinding.FragmentSummaryBinding
  */
 class SummaryFragment : Fragment() {
 
+    private val sharedViewModel: OrderViewModel by activityViewModels()
     // Binding object instance corresponding to the fragment_summary.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
@@ -30,9 +34,10 @@ class SummaryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding?.apply {
-            sendButton.setOnClickListener { sendOrder() }
+            viewModel = sharedViewModel
+            summaryFragment = this@SummaryFragment
+            lifecycleOwner = viewLifecycleOwner
         }
     }
 
